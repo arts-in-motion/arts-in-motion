6717cfdd396b47b6f943bccbe539eceac61e8b50
Jace Browning <jacebrowning@gmail.com>
2017-11-03 20:29:31 -0400

    Serve the admin from the root
    Closes #4

--- config/urls.py
+++ config/urls.py
@@ -1,17 +1,8 @@
 from django.conf.urls import url, include
 from django.contrib import admin
-from django.shortcuts import redirect
-
-
-def append_slash_and_redirect(request):
-    assert not request.path.endswith('/')
-    return redirect(request.path + '/', permanent=True)
 
 
 urlpatterns = [
-    url('^(?:api|admin).*[^/]$', append_slash_and_redirect),
-
+    url('^', admin.site.urls),
     url('^api/', include('portal.api.urls')),
-    url('^admin/', admin.site.urls),
-
 ]
