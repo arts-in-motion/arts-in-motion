d4c4ec43e6874b424a3314748580c08ee9855bce
Jace Browning <jacebrowning@gmail.com>
2017-11-04 09:21:34 -0400

    Rename the admin site

--- portal/core/admin.py
+++ portal/core/admin.py
@@ -2,4 +2,9 @@ from django.contrib import admin
 from django.contrib.sites.models import Site
 
 
+admin.site.site_header = "Arts in Motion Data Portal"
+admin.site.site_title = "Arts in Motion"
+admin.site.site_url = None  # Hide "view site" link
+admin.site.index_title = None  # Hide default "Site administration" text
+
 admin.site.unregister(Site)
