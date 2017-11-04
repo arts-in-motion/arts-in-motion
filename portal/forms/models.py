d9c0efc1bfa0212eacd143f0b0517a857a8871a7
Gage Vander Clay <gvanderclay@gmail.com>
2017-11-03 23:12:11 -0400

    add fields to form

--- portal/forms/models.py
+++ portal/forms/models.py
@@ -5,21 +5,25 @@ class Form(models.Model):
     BACKGROUND_CHECK = "background_check"
     ENROLLMENT_FORM = "enrollment_form"
     VOLUNTEER_FORM = "volunteer_form"
+    OTHER = "other"
 
     FORM_TYPE_CHOICES = (
         (BACKGROUND_CHECK, 'Background Check'),
         (ENROLLMENT_FORM, 'Enrollment Form'),
-        (VOLUNTEER_FORM, 'Volunteer Form')
+        (VOLUNTEER_FORM, 'Volunteer Form'),
+        (OTHER, 'Other')
 
     )
     name = models.CharField(max_length=100)
-    person = models.name = models.ForeignKeyField(
+    person = models.name = models.ForeignKey(
         'contacts.person', on_delete=models.CASCADE)
     formType = models.CharField(
-        choices=FORM_TYPE_CHOICES
+        choices=FORM_TYPE_CHOICES,
+        max_length=30
     )
     isFilled = models.BooleanField()
-    notes = models.CharField()
+    notes = models.CharField(max_length=1000)
 
     date = models.DateField()
-    lastEdited = models.DateField()
+    # TODO make this set auto based on edit
+    lastEdited = models.DateField(auto_now=True)
