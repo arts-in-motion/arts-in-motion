b9a62c8007fe7c77713f380cb16c5afe51f1c1e6
Gage Vander Clay <gvanderclay@gmail.com>
2017-11-03 22:59:06 -0400

    add forms model

--- portal/forms/models.py
+++ portal/forms/models.py
@@ -2,5 +2,24 @@ from django.db import models
 
 
 class Form(models.Model):
+    BACKGROUND_CHECK = "background_check"
+    ENROLLMENT_FORM = "enrollment_form"
+    VOLUNTEER_FORM = "volunteer_form"
 
+    FORM_TYPE_CHOICES = (
+        (BACKGROUND_CHECK, 'Background Check'),
+        (ENROLLMENT_FORM, 'Enrollment Form'),
+        (VOLUNTEER_FORM, 'Volunteer Form')
+
+    )
     name = models.CharField(max_length=100)
+    person = models.name = models.ForeignKeyField(
+        'contacts.person', on_delete=models.CASCADE)
+    formType = models.CharField(
+        choices=FORM_TYPE_CHOICES
+    )
+    isFilled = models.BooleanField()
+    notes = models.CharField()
+
+    date = models.DateField()
+    lastEdited = models.DateField()
