1a6226c91249451a9cf38d18c40e7faea99678fe
Rachell Calhoun <rachell.calhoun@gmail.com>
2017-11-04 19:01:15 -0400

    adding donations

---
+++ portal/donations/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+
+from portal.contacts.models import Donor
+from portal.events.models import Event
+
+class Donation(models.Model):
+
+    donor = models.ForeignKey(Donor)
+    amount = models.CharField(max_length=100)
+    date = models.DateField()
+    INKIND = 'In Kind'
+    CASH = 'Cash'
+    WEBSITE = 'Website'
+    CHECK = 'Check'
+    CREDIT_CARD = 'Credit Card'
+    KIND_CHOICES = (
+        (INKIND, 'In Kind'),
+        (CASH, 'Cash'),
+        (WEBSITE, 'Website'),
+        (CHECK, 'Check'),
+        (CREDIT_CARD, 'Credit Card'),
+    )
+    kind = models.CharField(choices=KIND_CHOICES, max_length=40)
+    notes = models.TextField(blank=True, null=True)
+    event = models.ForeignKey(Event, blank=True, null=True)
+
+    def __str__(self):
+        return f"{self.amount}"
