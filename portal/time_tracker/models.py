a173c9e7c955201ea90e5ffe1e07b66f1dc8ccf7
Allison Bolen <bolena@mail.gvsu.edu>
2017-11-03 23:51:15 -0400

    time-app and model info

---
+++ portal/time_tracker/models.py
@@ -0,0 +1,18 @@
+from django.db import models
+
+class Time_tracker(models.Model):
+
+    EVENT = "event"
+    CLASS = "class"
+    OTHER = "other"
+
+    TIME_TYPE_CHOICES = (
+        (EVENT, 'Event'),
+        (CLASS, 'Class'),
+        (OTHER, 'Other')
+    )
+
+    hours = models.IntegerField()
+    notes = models.charField(max_length=1000)
+    person = models.name = models.ForeignKey('contacts.person', on_delete=models.CASCADE)
+    date = models.DateField()
