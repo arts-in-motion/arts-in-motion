d2dd475923e6699956dba5bc22740cc5dda5dc96
Rachell Calhoun <rachell.calhoun@gmail.com>
2017-11-05 10:50:54 -0500

    hiding waystohelp from admin

--- portal/contacts/admin.py
+++ portal/contacts/admin.py
@@ -32,8 +32,13 @@ class StudentInline(SingleInline):
     )
 
 
-admin.site.register(models.WaysToHelp)
+@admin.register(models.WaysToHelp)
+class WaysToHelpAdmin(admin.ModelAdmin):
 
+    @staticmethod
+    def get_model_perms(_request):
+        """Hide this model, but make it available for search."""
+        return {}
 
 class VolunteerInline(SingleInline):
     model = models.Volunteer
