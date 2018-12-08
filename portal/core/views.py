e4a4e65a2b1a70c0d5c595ab46fd70f77c9c8a88
Rachell Calhoun <rachell.calhoun@gmail.com>
2018-12-08 13:28:58 -0500

    add reports for class list and detail class view

---
+++ portal/core/views.py
@@ -0,0 +1,16 @@
+
+from django.utils import timezone
+from django.shortcuts import render, get_object_or_404, redirect
+from portal.classes.models import Class
+from portal.contacts.models import Student
+from django.contrib.auth.decorators import login_required
+
+
+def class_reports(request):
+    classes = Class.objects.all()
+    return render(request,'core/class_reports.html',{'classes': classes})
+
+def detail_class_reports(request, class_id):
+    aim_class = get_object_or_404(Class, pk=class_id)
+    students = Student.objects.filter(classes__in=[aim_class])
+    return render(request, 'core/detail_class_reports.html', {'class': aim_class, 'students': students})
