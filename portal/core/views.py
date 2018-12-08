82fc0b47d5910a0c991bb212c4cec83e9ed79b55
Rachell Calhoun <rachell.calhoun@gmail.com>
2018-12-08 14:03:17 -0500

    add templates and print for class student list

--- portal/core/views.py
+++ portal/core/views.py
@@ -5,11 +5,12 @@ from portal.classes.models import Class
 from portal.contacts.models import Student
 from django.contrib.auth.decorators import login_required
 
-
+@login_required
 def class_reports(request):
     classes = Class.objects.all()
     return render(request,'core/class_reports.html',{'classes': classes})
 
+@login_required
 def detail_class_reports(request, class_id):
     aim_class = get_object_or_404(Class, pk=class_id)
     students = Student.objects.filter(classes__in=[aim_class])
